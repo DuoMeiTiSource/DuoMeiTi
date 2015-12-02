dc163c9502fa6c7e1110227462d681c6d11e141d
liuzhuocpp <liuzhuocpp@qq.com>
2015-12-02 16:15:01 +0800

    更新代码

--- js/classroom/classroomManage.js
+++ js/classroom/classroomManage.js
@@ -2,7 +2,7 @@
 
 $(document).on("click", ".delete", function(){
 	deleteID = $(this).parents("[classroomId]").attr("classroomId");
-	alert("delete:" + deleteID);
+	/*alert("delete:" + deleteID);*/
 	
 	$.ajax({
 		url : 'classroomManageNew_classroomDelete',
@@ -148,7 +148,7 @@ $(document).on("click", ".update", function (){
 	$('#classroom_modal').modal('show');
 	submit_type = "update";
 	classroomId = $(this).parents("[classroomId]").attr('classroomId');
-//	alert(classroomId);
+	alert(classroomId);
 	$("#input_principal_student_number").val($(this).parents("[studentNumber]").attr('studentNumber'));
 	$("#input_principal_student_full_name").text($(this).parents("[studentFullName]").attr('studentFullName'))
 	$("#input_classroom_num").val($(this).parents("[classroomNum]").attr('classroomNum'));
@@ -174,6 +174,12 @@ $(document).on("click", "#addClassroom", function (){
 	
 	studentNumber = $("#input_principal_student_number").val();
 	classroom_num = $("#input_classroom_num").val();
+	
+	if(classroom_num=="")
+	{
+		$("#exist").text("教室号为空，添加失败");
+		return ;
+	}
 	
 //	alert(classroom_num)
 //	studentId = 
@@ -201,9 +207,16 @@ function addClassroomCallback(data) {
 	else if(data.status == "ok") {
 		$('#classroom_modal').modal('hide');
 		$(".classroomTableDiv").html(data.classroomHtml);
-//		window.location.href=window.location.href;  
-//		window.location.reload;
 	}
+	else if(data.status == "no_principal")
+	{
+		$("#exist").text("负责人学号不正确，添加失败");
+	}
+	else
+	{
+		alert("发生严重错误，不知名原因");
+	}
+	
 }
 
 
